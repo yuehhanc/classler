c60b1a79832165ee39364fb69301254e48b26a1d
Ubuntu <ubuntu@ip-172-31-41-230.us-east-2.compute.internal>
2019-02-28 05:51:53 +0000

    Add multi-user support

--- classler/views.py
+++ classler/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.core.files import File
 import subprocess
+import time
+import os
+import shutil
 
 # Create your views here.
 def home(request):
@@ -15,15 +18,19 @@ def submit_code(request):
             'result': 'Cannot get code'
             }
     if code != None:
-        with open('/home/ubuntu/python/solution/solution.py', 'w') as f:
-            solution = File(f)
-            solution.write(code)
-        subprocess.call("/home/ubuntu/start_docker.sh", shell=True)
-        with open('/home/ubuntu/python/answer.log', 'r') as f:
+        root = '/home/ubuntu/python/'
+        folder = current_milli_time()
+        solution = root + folder
+        os.makedirs(solution)
+        with open(solution + '/solution.py', 'w') as f:
+            f.write(code)
+        subprocess.call("/home/ubuntu/start_docker.sh " + folder, shell=True)
+        with open(solution + '/answer.log', 'r') as f:
             ans = ""
             for line in f:
                 ans += line
             data['result'] = ans
+        #shutil.rmtree(solution)
 
     return JsonResponse(data)
 
@@ -32,3 +39,6 @@ def compileCode(request):
     # One of the ways to compile the code is to use "eval()"
     # we can directly code it in the surver and return the result as a http response
     pass
+
+def current_milli_time():
+    return str(int(round(time.time() * 1000)))

f25cd9937c92ceec15b2625a238d10223dd178a5
Ubuntu <ubuntu@ip-172-31-41-230.us-east-2.compute.internal>
2019-03-05 06:54:06 +0000

    integrate multi-docker support to solution framework

--- classler/views.py
+++ classler/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.core.files import File
+from pathlib import Path
 import subprocess
 import time
 import os
@@ -11,7 +12,7 @@ def home(request):
     context = {}
     return render(request, 'classler/home.html', context)
 
-def submit_code(request, problem="two_sum"):
+def submit_code_demo(request, problem="two_sum"):
     context = {}
     code = request.GET.get('code')
     data = {
@@ -35,7 +36,7 @@ def submit_code(request, problem="two_sum"):
 
     return JsonResponse(data)
 
-def two_sums_submit(request, problem):
+def submit_code(request, problem="two_sum"):
     context = {}
     code = request.GET.get('code')
     data = {
@@ -43,18 +44,24 @@ def two_sums_submit(request, problem):
             }
     # Let the front send the name of a problem. We want to use it to find the matching file.
     if code != None:
+        root = str(Path.home()) + '/python/'
+        folder = current_milli_time()
+        solution = root + folder
+        os.makedirs(solution)
         pre_code = "from solution_framework.solution import Solution\nimport sys\n\n"
-        post_code = "\n\nif __name__ == '__main__':\n    sol = Solution(1, 0.1)\n    result = sol.run(two_sum)\n    sys.stdout.flush()"
+        post_code = "\n\nif __name__ == '__main__':\n    sol = Solution(1, sys.argv[1], 0.1)\n    result = sol.run(two_sum)\n    sys.stdout.flush()"
         full_code = pre_code + code + post_code
-        with open('/Users/roland/python/solution/two_sum.py', 'w') as f:
-            solution = File(f)
-            solution.write(full_code)
-        subprocess.call("/Users/roland/start_docker.sh", shell=True)
-        with open('/Users/roland/python/answer.log', 'r') as f:
+        with open(solution + '/two_sum.py', 'w') as f:
+            f.write(full_code)
+        # docker run --rm --volumes-from test test cp /$HOME/python/$TIME/two_sum.py /$HOME/python/solution/ && python /$HOME/python/solution/two_sum.py $TIME
+        cmd = "docker run --rm --volumes-from test test cp " + solution + "/two_sum.py " + root + "solution/ && python " + root + "solution/two_sum.py " + solution
+        subprocess.call(cmd, shell=True)
+        with open(solution + '/answer.log', 'r') as f:
             ans = ""
             for line in f:
                 ans += line
             data['result'] = ans
+        #shutil.rmtree(solution)
 
     return JsonResponse(data)
 

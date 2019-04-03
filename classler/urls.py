be76a6a513d9d3a3da35ac8596ff87b108f53b48
Ubuntu <ubuntu@ip-172-31-30-153.us-east-2.compute.internal>
2019-04-03 02:35:21 +0000

    merging

--- classler/urls.py
+++ classler/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^submit_code$', views.submit_code, name='submit_code'),
-    url(r'^problems/(?P<problem_name>.+)/code-submit$', views.code_submit, name='code_submit'),
+    url(r'^problems/(?P<problem_name>.+)/code-submit$', views.submit_code, name='code_submit'),
 ]
--- classler/views.py
+++ classler/views.py
@@ -59,35 +59,43 @@ def submit_code_demo(request, problem="two_sum"):
 
     return JsonResponse(data)
 
-def submit_code(request, problem="two_sum"):
+@csrf_exempt
+def submit_code(request, problem):
     context = {}
     code = request.GET.get('code')
     data = {
             'result': 'Cannot get code'
             }
-    # Let the front send the name of a problem. We want to use it to find the matching file.
-    if code != None:
-        root = str(Path.home()) + '/python/'
-        folder = current_milli_time()
-        DOCKER_ROOT = '/home/ubuntu/python/'
-        docker_solution = DOCKER_ROOT + folder
-        solution = root + folder
-        print(getpass.getuser())
-        os.makedirs(solution)
-        pre_code = "from solution_framework.solution import Solution\nimport sys\n\n"
-        post_code = "\n\nif __name__ == '__main__':\n    sol = Solution(1, sys.argv[1], 0.1)\n    result = sol.run(two_sum)\n    sys.stdout.flush()"
-        full_code = pre_code + code + post_code
-        with open(solution + '/two_sum.py', 'w') as f:
-            f.write(full_code)
-        # docker run --rm --volumes-from test test cp /$HOME/python/$TIME/two_sum.py /$HOME/python/solution/ && python /$HOME/python/solution/two_sum.py $TIME
-        cmd = 'docker run --rm --volumes-from test test /bin/bash -c "cp ' + docker_solution + '/two_sum.py ' + DOCKER_ROOT + 'solution/ && python ' + DOCKER_ROOT + 'solution/two_sum.py ' + docker_solution + '"'
-        subprocess.call(cmd, shell=True)
-        with open(solution + '/answer.log', 'r') as f:
-            ans = ""
-            for line in f:
-                ans += line
-            data['result'] = ans
-        shutil.rmtree(solution)
+    try:
+        # Let the front send the name of a problem. We want to use it to find the matching file.
+        if code != None:
+            root = str(Path.home()) + '/python/'
+            folder = current_milli_time()
+            DOCKER_ROOT = '/home/ubuntu/python/'
+            docker_solution = DOCKER_ROOT + folder
+            solution = root + folder
+            print(getpass.getuser())
+            os.makedirs(solution)
+            pre_code = "from solution_framework.solution import Solution\nimport sys\n\n"
+            post_code = "\n\nif __name__ == '__main__':\n    sol = Solution(1, sys.argv[1], 0.1)\n    result = sol.run(two_sum)\n    sys.stdout.flush()"
+            full_code = pre_code + code + post_code
+            with open(solution + '/two_sum.py', 'w') as f:
+                f.write(full_code)
+            # docker run --rm --volumes-from test test cp /$HOME/python/$TIME/two_sum.py /$HOME/python/solution/ && python /$HOME/python/solution/two_sum.py $TIME
+            cmd = 'docker run --rm --volumes-from test test /bin/bash -c "cp ' + docker_solution + '/two_sum.py ' + DOCKER_ROOT + 'solution/ && python ' + DOCKER_ROOT + 'solution/two_sum.py ' + docker_solution + '"'
+            subprocess.call(cmd, shell=True)
+            with open(solution + '/answer.log', 'r') as f:
+                ans = ""
+                for line in f:
+                    ans += line
+                data['result'] = ans
+            shutil.rmtree(solution)
+    except Exception as e:
+        data = {"result": "Result: " + str(e),
+              "num_test_passed": "test passed: 0/0 tests",
+              "runtime": "Time: N/A",
+              }
+    # TODO: Change the dummy result in to a real one
 
     return Response(data)
 

ec80739b13a34f7a14683c58ee11c9d24f2c49a4
Yueh-Han <yuehhanc@andrew.cmu.edu>
2019-04-02 22:34:04 -0400

    Change ip

--- classler/static/ang/polyfills.js
+++ classler/static/ang/polyfills.js
@@ -5784,8 +5784,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/ubuntu/classler-env/classler/frontend/src/polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! /home/ubuntu/classler-env/classler/frontend/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! /Users/roland/Desktop/Program_Development/classler/frontend/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/roland/Desktop/Program_Development/classler/frontend/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
